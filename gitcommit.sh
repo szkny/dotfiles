2d680841cc0d60a5544abb387a41490b17e3866b
szkny <suzuki.s1008@gmail.com>
2018-05-10 17:10:48 +0900

    Thu May 10 17:10:48 DST 2018

--- gitcommit.sh
+++ gitcommit.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+CURRENT_BRANCH="`git symbolic-ref --short HEAD`"
 git add .
 git commit -m "`date`"
-git push -u origin "`git symbolic-ref --short HEAD`"
+git push -u origin $CURRENT_BRANCH
+git checkout master
+git merge $CURRENT_BRANCH
+git checkout $CURRENT_BRANCH
