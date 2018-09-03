458c1ca03da276cd4ef9a44ea91e118a42245eaf
szkny <suzuki.s1008@gmail.com>
2018-09-03 09:41:56 +0900

    Mon Sep  3 09:41:56 JST 2018

--- ipython/jupyter_qtconsole_config.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-c.JupyterWidget.syntax_style = "monokai"
-c.ConsoleWidget.include_other_output = True
---
+++ jupyter_qtconsole/jupyter_qtconsole_config.py
@@ -0,0 +1,13 @@
+c.JupyterWidget.syntax_style = "monokai"
+c.JupyterQtConsoleApp.hide_menubar = True
+c.ConsoleWidget.include_other_output = True
+# c.JupyterWidget.editor = 'term -- nvim'
+# c.InlineBackend.rc = {'font.family': 'Ricty Diminished'}
+# c.InteractiveShellApp.exec_lines = [
+#     'import numpy as np',
+#     'import matplotlib.pyplot as plt',
+#     'import pandas as pd',
+#     '%matplotlib inline',
+#     '%load_ext autoreload',
+#     '%autoreload 2'
+# ]
