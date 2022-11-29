50a574a76c0d6264c5e2e7ed2f32e34b7ea72a1d
szkny <suzuki.s1008@gmail.com>
2022-11-30 00:08:38 +0900

    2022年 11月30日 水曜日 00時08分38秒 JST

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
-# neovim
-if [ -L ~/.config/nvim/init.vim ]; then rm ~/.config/nvim/init.vim;fi
-if [ -L ~/.config/nvim/vimrc ]; then rm ~/.config/nvim/vimrc;fi
-if [ -L ~/.config/nvim/ftplugin ]; then rm ~/.config/nvim/ftplugin;fi
-mkdir -p ~/.config/nvim
-ln -sf ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
-ln -sf ~/dotfiles/nvim/vimrc ~/.config/nvim
-ln -sf ~/dotfiles/nvim/ftplugin ~/.config/nvim
+# # neovim
+# if [ -L ~/.config/nvim/init.vim ]; then rm ~/.config/nvim/init.vim;fi
+# if [ -L ~/.config/nvim/vimrc ]; then rm ~/.config/nvim/vimrc;fi
+# if [ -L ~/.config/nvim/ftplugin ]; then rm ~/.config/nvim/ftplugin;fi
+# mkdir -p ~/.config/nvim
+# ln -sf ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+# ln -sf ~/dotfiles/nvim/vimrc ~/.config/nvim
+# ln -sf ~/dotfiles/nvim/ftplugin ~/.config/nvim
 
 # # nyaovim
 # if [ -L ~/.config/nyaovim/nyaovimrc.html ]; then rm ~/.config/nyaovim/nyaovimrc.html;fi
 # mkdir -p ~/.config/nyaovim
 # ln -sf ~/dotfiles/nyaovim/nyaovimrc.html ~/.config/nyaovim/nyaovimrc.html
 
-# python syntax checker & formatter
-if [ -L ~/.config/flake8 ]; then rm ~/.config/flake8;fi
-if [ -L ~/.config/yapf ]; then rm ~/.config/yapf;fi
-mkdir -p ~/.config
-ln -sf ~/dotfiles/python_syntax_checker/flake8 ~/.config/flake8
-ln -sf ~/dotfiles/python_syntax_checker/yapf ~/.config
+# # python syntax checker & formatter
+# if [ -L ~/.config/flake8 ]; then rm ~/.config/flake8;fi
+# if [ -L ~/.config/yapf ]; then rm ~/.config/yapf;fi
+# mkdir -p ~/.config
+# ln -sf ~/dotfiles/python_syntax_checker/flake8 ~/.config/flake8
+# ln -sf ~/dotfiles/python_syntax_checker/yapf ~/.config
 
 # # vim (8.0.1283)
 # if [ -L ~/.vimrc ]; then rm ~/.vimrc;fi
--- gitcommit.sh
+++ gitcommit.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 CURRENT_BRANCH="`git symbolic-ref --short HEAD`"
-git add .
+git add -A
 git commit -m "`date`"
 git push -u origin $CURRENT_BRANCH
-# git checkout master
+# git checkout main
 # git pull
 # git merge $CURRENT_BRANCH
-# git push -u origin master
+# git push -u origin main
 # git checkout $CURRENT_BRANCH
