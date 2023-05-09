83d5794ffb6b90679d2fcedaff433cd168853135
Sohei.Suzuki <suzuki.s1008@gmail.com>
2023-05-10 00:24:52 +0900

    add Trans command for init.lua

--- nvim/lua/util.lua
+++ nvim/lua/util.lua
@@ -29,3 +29,90 @@ vim.cmd([[
         return l:now
     endf
 ]])
+
+vim.cmd([[
+    fun! s:trans(...) abort range
+        " transコマンド(Google翻訳)を利用してvisual選択中の文字列を日本語変換する関数
+        if executable('trans')
+            let l:text = ''
+            if a:0 == 0
+                let @@ = ''
+                exe 'silent normal gvy'
+                if @@ !=# ''
+                    let l:text = join(split(@@,'\n'))
+                else
+                    let l:text = expand('<cword>')
+                endif
+            else
+                let l:text = join(a:000)
+            endif
+            let l:text = substitute(l:text, '"', '\\"', 'g')
+            if len(l:text) < 900
+                call splitterm#open('trans', '{en=ja}', '"'.l:text.'"', ' ; read -q')
+            else
+                echo 'Trans: [error] text too long.'
+            endif
+        else
+            call s:install_trans()
+        endif
+    endf
+    command! -range -nargs=* Trans call s:trans(<f-args>)
+
+
+    fun! s:transja(...) abort range
+        " transコマンド(Google翻訳)を利用してvisual選択中の日本語を英語に変換する関数
+        if executable('trans')
+            let l:text = ''
+            if a:0 == 0
+                let @@ = ''
+                exe 'silent normal gvy'
+                if @@ !=# ''
+                    let l:text = join(split(@@,'\n'))
+                else
+                    let l:text = expand('<cword>')
+                endif
+            else
+                let l:text = join(a:000)
+            endif
+            let l:text = substitute(l:text, '"', '\\"', 'g')
+            if len(l:text) < 900
+                call splitterm#open('trans', '{ja=en}', '"'.l:text.'"', ' ; read -q')
+            else
+                echo 'Trans: [error] text too long.'
+            endif
+        else
+            call s:install_trans()
+        endif
+    endf
+    command! -range -nargs=* Transja call s:transja(<f-args>)
+
+
+    fun! s:install_trans() abort
+        if has('mac')
+            let l:install_cmd = 'brew install http://www.soimort.org/translate-shell/translate-shell.rb'
+        elseif system('uname') ==# "Linux\n"
+            let l:exe = '/usr/local/bin/trans'
+            let l:install_cmd = 'sudo wget git.io/trans -O '.l:exe
+            let l:install_cmd .= ' && sudo chmod +x '.l:exe
+        else
+            echon 'Trans: [error] trans command not found.'
+            return
+        endif
+        silent call splitterm#open('(echo "trans command not found. installing ..."'
+                                 \.' && '.l:install_cmd
+                                 \.' && echo " Success !!"'
+                                 \.' && echo " you can do \":Trans [WORD]\".")'
+                                 \.' ; read -q')
+        startinsert
+    endf
+]])
+
+vim.cmd([[
+    fun! s:open() abort
+        " openコマンドにより編集中ファイルを開く関数
+        if executable('open')
+            silent exe '!open '.expand('%:p')
+        endif
+    endf
+    command! Open call s:open()
+]])
