b4f29bb21bc56aa0e7c16c09bea53c5f2942b780
Sohei.Suzuki <suzuki.s1008@gmail.com>
2023-08-28 11:20:34 +0900

    tweak nvim-lsp

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -20,7 +20,7 @@ require('mason-lspconfig').setup_handlers({ function(server)
   require('lspconfig')[server].setup(opt)
 end })
 
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
