ae374fa0c19f504a0d453e4ba3e991a8d37a808e
Sohei.Suzuki <sohei.suzuki@accenture.com>
2023-08-28 10:23:57 +0900

    WIP: setup mason/nvim-lspconfig

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -14,6 +14,11 @@ require('mason-lspconfig').setup_handlers({ function(server)
   require('lspconfig')[server].setup(opt)
 end })
 
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
 
 local cmp = require("cmp")
 cmp.setup({
