63626a8b1691b364c3730ca0bc45bd7799dcc1ea
Sohei.Suzuki <suzuki.s1008@gmail.com>
2024-01-27 16:21:42 +0900

    tweak auto-session setup

--- nvim/lua/plugin_config.lua
+++ nvim/lua/plugin_config.lua
@@ -284,7 +284,7 @@ require("auto-session").setup({
   auto_session_root_dir = vim.fn.stdpath("data").."/sessions/",
   auto_save_enabled = true,
   auto_restore_enabled = true,
-  auto_session_enable_last_session = true,
+  auto_session_enable_last_session = false,
   auto_session_suppress_dirs = { "~/", "~/Project", "~/Downloads", "/" },
   cwd_change_handling = {
     restore_upcoming_session = true,
