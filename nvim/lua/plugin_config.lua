c7a72e68a04ace61324e04f58ab3a94aef558339
Sohei.Suzuki <suzuki.s1008@gmail.com>
2024-01-29 00:41:23 +0900

    tweak nvim.conf

--- nvim/lua/plugin_config.lua
+++ nvim/lua/plugin_config.lua
@@ -282,16 +282,18 @@ vim.cmd([[
 require("auto-session").setup({
   log_level = "error",
   auto_session_root_dir = vim.fn.stdpath("data").."/sessions/",
-  auto_save_enabled = true,
-  auto_restore_enabled = true,
+  auto_session_enabled=true,
+  auto_session_create_enabled=true,
   auto_session_enable_last_session = false,
   auto_session_suppress_dirs = { "~/", "~/Project", "~/Downloads", "/" },
+  auto_save_enabled = true,
+  auto_restore_enabled = true,
+  auto_session_use_git_branch=false,
+  bypass_session_save_file_types = nil,
   cwd_change_handling = {
     restore_upcoming_session = true,
     pre_cwd_changed_hook = nil,
-    post_cwd_changed_hook = function()
-      require("lualine").refresh()
-    end,
+    post_cwd_changed_hook = nil,
   },
 })
 vim.api.nvim_create_autocmd({ 'BufEnter' }, {
