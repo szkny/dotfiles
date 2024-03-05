a6505fd17fd427635102bec856079458be85841a
Sohei.Suzuki <suzuki.s1008@gmail.com>
2024-03-05 21:57:07 +0900

    tweak nvim lsp diagnostic config

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -115,7 +115,10 @@ end, { bang = true, nargs = "?" })
 -- LSP handlers
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
   update_in_insert = false,
+  signs = true,
+  underline = true,
   virtual_text = {
+    prefix = "",
     format = function(diagnostic)
       return string.format("%s (%s: %s)", diagnostic.message, diagnostic.source, diagnostic.code)
     end,
