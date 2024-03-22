4f52b9e7a137b278685312c3b4d52593c7da0389
Sohei.Suzuki <sohei.suzuki@accenture.com>
2024-03-22 13:59:43 +0900

    fix: fuzzy-motion.vim lazy load setup

--- nvim/lua/plugins/fuzzy_motion.lua
+++ nvim/lua/plugins/fuzzy_motion.lua
@@ -1,15 +1,15 @@
 return {
-  "yuki-yano/fuzzy-motion.vim",
-  dependencies = { "vim-denops/denops.vim" },
-  keys = {
-    { "<Leader>f", "<CMD>silent FuzzyMotion<CR>", mode = "n" },
-    { "s",         "<CMD>silent FuzzyMotion<CR>", mode = "n" },
-  },
-  config = function()
-    vim.g.fuzzy_motion_auto_jump = true
-    vim.api.nvim_set_hl(0, "FuzzyMotionShade", { fg = "#666666", bg = "none", bold = true })
-    vim.api.nvim_set_hl(0, "FuzzyMotionChar", { fg = "#eeeeee", bg = "#ff3377", bold = true })
-    vim.api.nvim_set_hl(0, "FuzzyMotionSubChar", { fg = "#eeeeee", bg = "#ff3377", bold = true })
-    vim.api.nvim_set_hl(0, "FuzzyMotionMatch", { fg = "#66bbee", bg = "none", bold = true })
-  end,
+	"yuki-yano/fuzzy-motion.vim",
+	dependencies = { "vim-denops/denops.vim" },
+	event = "VeryLazy",
+	config = function()
+		vim.g.fuzzy_motion_auto_jump = true
+		local opts = { noremap = true, silent = true }
+		vim.keymap.set("n", "<Leader>f", "<CMD>FuzzyMotion<CR>", opts)
+		vim.keymap.set("n", "s", "<CMD>FuzzyMotion<CR>", opts)
+		vim.api.nvim_set_hl(0, "FuzzyMotionShade", { fg = "#666666", bg = "none", bold = true })
+		vim.api.nvim_set_hl(0, "FuzzyMotionChar", { fg = "#eeeeee", bg = "#ff3377", bold = true })
+		vim.api.nvim_set_hl(0, "FuzzyMotionSubChar", { fg = "#eeeeee", bg = "#ff3377", bold = true })
+		vim.api.nvim_set_hl(0, "FuzzyMotionMatch", { fg = "#66bbee", bg = "none", bold = true })
+	end,
 }
