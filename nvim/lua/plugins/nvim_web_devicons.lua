80b40a04adef4e52237ec2666e50a794bb147b36
Sohei.Suzuki <suzuki.s1008@gmail.com>
2024-03-24 02:19:07 +0900

    tweak neovim setuo

--- nvim/lua/plugins/nvim_web_devicons.lua
+++ nvim/lua/plugins/nvim_web_devicons.lua
@@ -1,33 +1,32 @@
 return {
-  "nvim-tree/nvim-web-devicons",
-  dependencies = { "Mofiqul/vscode.nvim" },
-  opts = {
-    override_by_extension = {
-      ["log"] = {
-        icon = "",
-        color = "#81e043",
-        name = "Log",
-      },
-      ["txt"] = {
-        icon = "",
-        color = "#81e043",
-        name = "Text",
-      },
-      ["csv"] = {
-        icon = "",
-        color = "#81e043",
-        name = "Csv",
-      },
-      ["tsv"] = {
-        icon = "",
-        color = "#81e043",
-        name = "Tsv",
-      },
-      ["dockerfile"] = {
-        icon = "",
-        color = "#5599dd",
-        name = "Dockerfile",
-      },
-    },
-  },
+	"nvim-tree/nvim-web-devicons",
+	opts = {
+		override_by_extension = {
+			["log"] = {
+				icon = "",
+				color = "#81e043",
+				name = "Log",
+			},
+			["txt"] = {
+				icon = "",
+				color = "#81e043",
+				name = "Text",
+			},
+			["csv"] = {
+				icon = "",
+				color = "#81e043",
+				name = "Csv",
+			},
+			["tsv"] = {
+				icon = "",
+				color = "#81e043",
+				name = "Tsv",
+			},
+			["dockerfile"] = {
+				icon = "",
+				color = "#5599dd",
+				name = "Dockerfile",
+			},
+		},
+	},
 }
