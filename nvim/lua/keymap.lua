b4ecf7097b29d0ae501561f9802af845a865df01
Sohei.Suzuki <suzuki.s1008@gmail.com>
2024-03-23 18:29:11 +0900

    tweak <Leader>d keymap

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -11,167 +11,178 @@ vim.g.maplocalleader = " "
 
 -- general
 local function reload_nvim_config()
-  local dir = vim.fn.expand("~/.config/nvim/lua")
-  local files = vim.fn.readdir(dir)
-  for _, file in ipairs(files) do
-    if vim.fn.isdirectory(dir .. "/" .. file) == 0 and file:match("%.lua$") then
-      if file ~= "plugin_core.lua" then
-        dofile(dir .. "/" .. file)
-      end
-    end
-  end
-  print("Nvim configurations reloaded!")
+	local dir = vim.fn.expand("~/.config/nvim/lua")
+	local files = vim.fn.readdir(dir)
+	for _, file in ipairs(files) do
+		if vim.fn.isdirectory(dir .. "/" .. file) == 0 and file:match("%.lua$") then
+			if file ~= "plugin_core.lua" then
+				dofile(dir .. "/" .. file)
+			end
+		end
+	end
+	print("Nvim configurations reloaded!")
 end
 keymap("n", "<leader><leader>", reload_nvim_config, opts)
 keymap("t", "<C-[>", "<C-\\><C-n>", opts)
 keymap("t", "<ESC>", "<C-\\><C-n><Plug>(esc)", opts)
 keymap("n", "<Plug>(esc)<ESC>", "i<ESC>", opts)
 keymap("i", "<C-s>", function()
-  Try({
-    function()
-      vim.cmd("w!")
-    end,
-    Catch({
-      function(err)
-        print("caught error: " .. err)
-      end,
-    }),
-  })
+	Try({
+		function()
+			vim.cmd("w!")
+		end,
+		Catch({
+			function(err)
+				print("caught error: " .. err)
+			end,
+		}),
+	})
 end, opts)
 keymap("n", "<C-s>", function()
-  Try({
-    function()
-      vim.cmd("w!")
-    end,
-    Catch({
-      function(err)
-        print("caught error: " .. err)
-      end,
-    }),
-  })
+	Try({
+		function()
+			vim.cmd("w!")
+		end,
+		Catch({
+			function(err)
+				print("caught error: " .. err)
+			end,
+		}),
+	})
 end, opts)
 keymap("n", "q", function()
-  Try({
-    function()
-      vim.cmd("q")
-    end,
-    Catch({
-      function(error)
-        print("caught error: " .. error)
-        -- print('E173: some files to edit')
-      end,
-    }),
-  })
+	Try({
+		function()
+			vim.cmd("q")
+		end,
+		Catch({
+			function(error)
+				print("caught error: " .. error)
+				-- print('E173: some files to edit')
+			end,
+		}),
+	})
 end, opts)
 keymap("n", "<S-q>", function()
-  vim.cmd("qall!")
+	vim.cmd("qall!")
 end, opts)
 
 -- for edit
 keymap("v", ">", ">gv", opts)
 keymap("v", "<", "<gv", opts)
-vim.api.nvim_set_keymap(
-  "n",
-  "<Leader>d",
-  "ReplaceWordText()  !=# '' ? ':<C-u>'.  ReplaceWordText().'<Left><Left><Left>' : '<ESC>'",
-  { expr = true }
-)
-vim.api.nvim_set_keymap(
-  "v",
-  "<Leader>d",
-  "VReplaceWordText() !=# '' ? ':<C-u>'. VReplaceWordText().'<Left><Left><Left>' : '<ESC>'",
-  { expr = true }
-)
+keymap("n", "<Leader>d", function()
+	vim.cmd([[
+    let wordUnderCursor = expand("<cword>")
+    call inputsave()
+    let wordToReplace = input("Replace : ", wordUnderCursor)
+    call inputrestore()
+    call inputsave()
+    let replacement = input("Replace \"" . wordToReplace . "\" with: ")
+    call inputrestore()
+    let choice = confirm(
+        \ "Will you replace '".wordToReplace."' with '".replacement."' ?",
+        \ "&Yes\n&No\n&Check")
+    if choice == 1
+      exe line('.').',$s/'.wordToReplace.'/'.replacement.'/g'
+    elseif choice == 3
+      exe line('.').',$s/'.wordToReplace.'/'.replacement.'/gc'
+    endif
+  ]])
+end, opts)
+keymap("v", "<Leader>d", function()
+	vim.cmd([[
+    let wordUnderCursor = GetVselectTxt()
+    exe "normal \<ESC>"
+    call inputsave()
+    let wordToReplace = input("Replace : ", wordUnderCursor)
+    call inputrestore()
+    call inputsave()
+    let replacement = input("Replace \"" . wordToReplace . "\" with: ")
+    call inputrestore()
+    let choice = confirm(
+        \ "Will you replace '".wordToReplace."' with '".replacement."' ?",
+        \ "&Yes\n&No\n&Check")
+    if choice == 1
+      exe line('.').',$s/'.wordToReplace.'/'.replacement.'/g'
+    elseif choice == 3
+      exe line('.').',$s/'.wordToReplace.'/'.replacement.'/gc'
+    endif
+  ]])
+end, opts)
 vim.cmd([[
-    fun! s:get_vselect_txt()
-        if mode()=="v"
-            let [line_start, column_start] = getpos("v")[1:2]
-            let [line_end, column_end] = getpos(".")[1:2]
-        else
-            let [line_start, column_start] = getpos("'<")[1:2]
-            let [line_end, column_end] = getpos("'>")[1:2]
-        end
-        if (line2byte(line_start)+column_start) > (line2byte(line_end)+column_end)
-            let [line_start, column_start, line_end, column_end] =
-            \   [line_end, column_end, line_start, column_start]
-        end
-        let lines = getline(line_start, line_end)
-        if len(lines) == 0
-                return ''
-        endif
-        let lines[-1] = lines[-1][: column_end - 1]
-        let lines[0] = lines[0][column_start - 1:]
-        return join(lines, "\n")
-    endf
-    fun! ReplaceWordText() abort
-        let l:target = expand('<cword>')
-        if l:target !=# ''
-            return line('.').',$s/'.l:target.'//gc'
-        else
-            return ''
-        endif
-    endf
-    fun! VReplaceWordText() abort range
-        let l:target = s:get_vselect_txt()
-        if l:target !=# ''
-            return line('.').',$s/'.l:target.'//gc'
-        else
-            return ''
-        endif
-    endf
+  fun! GetVselectTxt()
+    if mode()=="v"
+      let [line_start, column_start] = getpos("v")[1:2]
+      let [line_end, column_end] = getpos(".")[1:2]
+    else
+      let [line_start, column_start] = getpos("'<")[1:2]
+      let [line_end, column_end] = getpos("'>")[1:2]
+    end
+    if (line2byte(line_start)+column_start) > (line2byte(line_end)+column_end)
+      let [line_start, column_start, line_end, column_end] =
+      \   [line_end, column_end, line_start, column_start]
+    end
+    let lines = getline(line_start, line_end)
+    if len(lines) == 0
+      return ''
+    endif
+    let lines[-1] = lines[-1][: column_end - 1]
+    let lines[0] = lines[0][column_start - 1:]
+    return join(lines, "\n")
+  endf
 ]])
 keymap("n", "<leader>r", function()
-  vim.cmd([[
-            fun! Rg_to_qf(line)
-                let parts = split(a:line, ':')
-                return {'filename': parts[0], 'lnum': parts[1], 'col': parts[2],
-                      \ 'text': join(parts[3:], ':')}
-            endf
+	vim.cmd([[
+    fun! Rg_to_qf(line)
+      let parts = split(a:line, ':')
+      return {'filename': parts[0], 'lnum': parts[1], 'col': parts[2],
+            \ 'text': join(parts[3:], ':')}
+    endf
 
-            fun! RgToQF(query)
-              call setqflist(map(systemlist('rg --column '.a:query), 'Rg_to_qf(v:val)'))
-            endf
+    fun! RgToQF(query)
+      call setqflist(map(systemlist('rg --column '.a:query), 'Rg_to_qf(v:val)'))
+    endf
 
-            let wordUnderCursor = expand("<cword>")
-            call inputsave()
-            let wordToReplace = input("Replace : ", wordUnderCursor)
-            call inputrestore()
-            call inputsave()
-            let replacement = input("Replace \"" . wordToReplace . "\" with: ")
-            call inputrestore()
-            call RgToQF(wordUnderCursor)
-            let numqf = len(getqflist())
-            let choice = confirm(
-                \ "Will you replace ".numqf." of '".wordToReplace."' with '".replacement."' ?",
-                \ "&Yes\n&No\n&Check")
-            if choice == 1
-                exe "cdo s/" . wordToReplace . "/" . replacement ."/g | w"
-            elseif choice == 3
-                exe "cdo s/" . wordToReplace . "/" . replacement ."/gc | w"
-            endif
-        ]])
-  -- local qflist = vim.fn.getqflist()
-  -- if #qflist > 0 then
-  --     local targetword = vim.fn.input("Target Word: ")
-  --     local replaceword = vim.fn.input("New Word: ")
-  --     local numreplace = 0
-  --     for i = 1,#qflist do
-  --         if string.find(qflist[i]["text"], targetword) then
-  --             numreplace = numreplace + 1
-  --         end
-  --     end
-  --     if numreplace > 0 then
-  --         local choice = vim.fn.confirm(
-  --             "Will you replace "..numreplace.." of '"..targetword.."' with '"..replaceword.."' ?",
-  --             "&Yes\n&No"
-  --         )
-  --         if choice == 1 then
-  --             vim.cmd("cdo s/"..targetword.."/"..replaceword.."/g | :w!")
-  --         end
-  --     else
-  --         print("not found: '"..targetword.."'")
-  --     end
-  -- end
+    let wordUnderCursor = expand("<cword>")
+    call inputsave()
+    let wordToReplace = input("Replace : ", wordUnderCursor)
+    call inputrestore()
+    call inputsave()
+    let replacement = input("Replace \"" . wordToReplace . "\" with: ")
+    call inputrestore()
+    call RgToQF(wordUnderCursor)
+    let numqf = len(getqflist())
+    let choice = confirm(
+        \ "Will you replace ".numqf." of '".wordToReplace."' with '".replacement."' ?",
+        \ "&Yes\n&No\n&Check")
+    if choice == 1
+      exe "cdo s/" . wordToReplace . "/" . replacement ."/g | w"
+    elseif choice == 3
+      exe "cdo s/" . wordToReplace . "/" . replacement ."/gc | w"
+    endif
+  ]])
+	-- local qflist = vim.fn.getqflist()
+	-- if #qflist > 0 then
+	--     local targetword = vim.fn.input("Target Word: ")
+	--     local replaceword = vim.fn.input("New Word: ")
+	--     local numreplace = 0
+	--     for i = 1,#qflist do
+	--         if string.find(qflist[i]["text"], targetword) then
+	--             numreplace = numreplace + 1
+	--         end
+	--     end
+	--     if numreplace > 0 then
+	--         local choice = vim.fn.confirm(
+	--             "Will you replace "..numreplace.." of '"..targetword.."' with '"..replaceword.."' ?",
+	--             "&Yes\n&No"
+	--         )
+	--         if choice == 1 then
+	--             vim.cmd("cdo s/"..targetword.."/"..replaceword.."/g | :w!")
+	--         end
+	--     else
+	--         print("not found: '"..targetword.."'")
+	--     end
+	-- end
 end, opts)
 
 -- -- for tab/window
