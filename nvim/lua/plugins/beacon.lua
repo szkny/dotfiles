ac6540f7b26fcb40542cb9afaca787aaea45f7b4
Sohei.Suzuki <suzuki.s1008@gmail.com>
2024-03-25 02:42:16 +0900

    add beacon.lua

---
+++ nvim/lua/plugins/beacon.lua
@@ -0,0 +1,23 @@
+return {
+	"danilamihailov/beacon.nvim",
+	cmd = {
+		"Beacon",
+		"BeaconToggle",
+		"BeaconOn",
+		"BeaconOff",
+	},
+	config = function()
+		-- local kopts = { noremap = true, silent = true }
+		-- vim.keymap.set("n", "n", "n<CMD>Beacon<CR>", kopts)
+		-- vim.keymap.set("n", "N", "N<CMD>Beacon<CR>", kopts)
+		-- vim.keymap.set("n", "*", "*<CMD>Beacon<CR>", kopts)
+		-- vim.keymap.set("n", "#", "#<CMD>Beacon<CR>", kopts)
+
+		vim.g.beacon_size = 40
+		vim.g.beacon_show_jumps = 1
+		vim.g.beacon_shrink = 1
+		vim.g.beacon_fade = 1
+		vim.g.beacon_timeout = 500
+		vim.api.nvim_set_hl(0, "Beacon", { bg = "#ffffff" })
+	end,
+}
