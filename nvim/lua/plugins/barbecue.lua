884046c73a5e0dde962b5381b96cd532a2770159
sohei.suzuki <suzuki.s1008@gmail.com>
2024-03-28 18:33:37 +0900

    tweak barbecue.lua setup

--- nvim/lua/plugins/barbecue.lua
+++ nvim/lua/plugins/barbecue.lua
@@ -1,25 +1,29 @@
 return {
-  "utilyre/barbecue.nvim",
-  dependencies = {
-    "SmiteshP/nvim-navic",
-    "nvim-tree/nvim-web-devicons",
-  },
-  event = "VeryLazy",
-  config = function()
-    require("barbecue").setup({
-      create_autocmd = false,
-    })
-    vim.api.nvim_create_autocmd({
-      "WinScrolled",
-      "BufWinEnter",
-      "CursorHold",
-      "InsertLeave",
-      "BufModifiedSet",
-    }, {
-      group = vim.api.nvim_create_augroup("barbecue.updater", {}),
-      callback = function()
-        require("barbecue.ui").update()
-      end,
-    })
-  end,
+	"utilyre/barbecue.nvim",
+	dependencies = {
+		"SmiteshP/nvim-navic",
+		"nvim-tree/nvim-web-devicons",
+	},
+	event = "VeryLazy",
+	opts = {
+		create_autocmd = false,
+		theme = {
+			normal = { bg = "none" },
+		},
+	},
+	config = function(_, opts)
+		require("barbecue").setup(opts)
+		vim.api.nvim_create_autocmd({
+			"WinScrolled",
+			"BufWinEnter",
+			"CursorHold",
+			"InsertLeave",
+			"BufModifiedSet",
+		}, {
+			group = vim.api.nvim_create_augroup("barbecue.updater", {}),
+			callback = function()
+				require("barbecue.ui").update()
+			end,
+		})
+	end,
 }
