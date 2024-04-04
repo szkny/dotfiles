c817bf4270ffe8c4a311cd054d06376f8353aca5
Sohei.Suzuki <sohei.suzuki@accenture.com>
2024-04-04 18:36:48 +0900

    tweak vim-easy-align keymap

--- nvim/lua/plugins/vim_easy_align.lua
+++ nvim/lua/plugins/vim_easy_align.lua
@@ -1,9 +1,9 @@
 return {
-  "junegunn/vim-easy-align",
-  event = "VeryLazy",
-  config = function()
-    vim.g.easy_align_ignore_groups = { "String" }
-    vim.keymap.set("v", "<Leader>=", "<CMD>EasyAlign *=<CR>", { noremap = true, silent = true })
-    vim.keymap.set("v", "<Enter>", "<Plug>(EasyAlign)", { noremap = true, silent = true })
-  end,
+	"junegunn/vim-easy-align",
+	event = "VeryLazy",
+	config = function()
+		vim.g.easy_align_ignore_groups = { "String" }
+		vim.keymap.set("v", "<Leader>=", ":'<,'>EasyAlign *=<CR>", { noremap = true, silent = true })
+		vim.keymap.set("v", "<Enter>", "<Plug>(EasyAlign)", { noremap = true, silent = true })
+	end,
 }
