31ecc31a042062a54bba99b8ab3adfdc7b8145cf
Sohei.Suzuki <suzuki.s1008@gmail.com>
2024-04-29 22:24:02 +0900

    tweak treesitter

--- nvim/lua/plugins/nvim_treesitter.lua
+++ nvim/lua/plugins/nvim_treesitter.lua
@@ -1,41 +1,45 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	event = "VimEnter",
-	opts = {
-		highlight = {
-			enable = true,
-			disable = {
-				"haskell",
-				"markdown",
-				"vim",
-				"terraform",
-				"terraform-vars",
-			},
-			additional_vim_regex_highlighting = false,
-		},
-		ensure_installed = {
-			"vim",
-			"regex",
-			"json",
-			"markdown",
-			"c",
-			"cpp",
-			"python",
-			"go",
-			"javascript",
-			"typescript",
-			"vue",
-			"css",
-		},
-		sync_install = false,
-		auto_install = true,
-		ignore_install = { "haskell", "help" },
-		indent = {
-			enable = true,
-		},
-	},
-	config = function(_, opts)
-		require("nvim-treesitter.configs").setup(opts)
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  event = "VimEnter",
+  opts = {
+    highlight = {
+      enable = true,
+      disable = {
+        "haskell",
+        "markdown",
+        "vim",
+        "terraform",
+        "terraform-vars",
+      },
+      additional_vim_regex_highlighting = false,
+    },
+    ensure_installed = {
+      "vim",
+      "regex",
+      "json",
+      "markdown",
+      "c",
+      "cpp",
+      "cmake",
+      "gitignore",
+      "python",
+      "go",
+      "javascript",
+      "typescript",
+      "vue",
+      "html",
+      "css",
+      "http",
+    },
+    sync_install = false,
+    auto_install = true,
+    ignore_install = { "haskell", "help" },
+    indent = {
+      enable = true,
+    },
+  },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
