86ef6e231fd7b1673e64252cd0523d2d3d87980c
Sohei.Suzuki <suzuki.s1008@gmail.com>
2024-05-01 16:25:19 +0900

    tweak dashboard-nvimdelete unnecessary code

--- nvim/lua/plugins/dashboard_nvim.lua
+++ nvim/lua/plugins/dashboard_nvim.lua
@@ -107,31 +107,5 @@ return {
 				},
 			},
 		})
-		-- vim.api.nvim_create_user_command("DashboardImage", function()
-		-- 	vim.cmd([[
-		--       " Show image in dashboard using ansi escape sequences
-		--       function! DashboardImage()
-		--         let s:width = 120
-		--         let s:height = 17
-		--         let s:row = float2nr(s:height / 5)
-		--         let s:col = float2nr((&columns - s:width) / 2)
-		--         let s:opts = {
-		--           \ 'relative': 'editor',
-		--           \ 'row': s:row,
-		--           \ 'col': s:col,
-		--           \ 'width': s:width,
-		--           \ 'height': s:height,
-		--           \ 'style': 'minimal'
-		--           \ }
-		--         let s:buf = nvim_create_buf(v:false, v:true)
-		--         let s:win = nvim_open_win(s:buf, v:true, s:opts)
-		--         hi! DashboardImage guibg=NONE guifg=NONE
-		--         call nvim_win_set_option(s:win, "winblend", 0)
-		--         call nvim_win_set_option(s:win, "winhl", "Normal:DashboardImage")
-		--         terminal cat ~/Downloads/ascii.txt
-		--         :exe "normal \<C-W>\<C-w>"
-		--       endfunction
-		--     ]])
-		-- end, {})
 	end,
 }
