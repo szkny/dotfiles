63f751248fba2287c93a499070154805b8a32868
Sohei.Suzuki <suzuki.s1008@gmail.com>
2024-05-01 16:22:24 +0900

    tweak dashboard-nvim

--- nvim/lua/plugins/dashboard_nvim.lua
+++ nvim/lua/plugins/dashboard_nvim.lua
@@ -35,9 +35,15 @@ return {
 			-- 	file_height = 20, -- preview file height
 			-- 	file_width = 130, -- preview file width
 			-- },
+			-- preview = {
+			-- 	command = "viu -x 25 -t ~/dotfiles/nvim/logo/nvim.png; read -q", -- preview command
+			-- 	file_path = "", -- preview file path
+			-- 	file_height = 15, -- preview file height
+			-- 	file_width = 80, -- preview file width
+			-- },
 			preview = {
-				command = "viu -x 25 -t ~/dotfiles/nvim/logo/nvim.png; read -q", -- preview command
-				file_path = "", -- preview file path
+				command = "cat", -- preview command
+				file_path = "~/dotfiles/nvim/logo/nvim-ascii.txt ; read -q", -- preview file path
 				file_height = 15, -- preview file height
 				file_width = 80, -- preview file width
 			},
@@ -101,5 +107,31 @@ return {
 				},
 			},
 		})
+		-- vim.api.nvim_create_user_command("DashboardImage", function()
+		-- 	vim.cmd([[
+		--       " Show image in dashboard using ansi escape sequences
+		--       function! DashboardImage()
+		--         let s:width = 120
+		--         let s:height = 17
+		--         let s:row = float2nr(s:height / 5)
+		--         let s:col = float2nr((&columns - s:width) / 2)
+		--         let s:opts = {
+		--           \ 'relative': 'editor',
+		--           \ 'row': s:row,
+		--           \ 'col': s:col,
+		--           \ 'width': s:width,
+		--           \ 'height': s:height,
+		--           \ 'style': 'minimal'
+		--           \ }
+		--         let s:buf = nvim_create_buf(v:false, v:true)
+		--         let s:win = nvim_open_win(s:buf, v:true, s:opts)
+		--         hi! DashboardImage guibg=NONE guifg=NONE
+		--         call nvim_win_set_option(s:win, "winblend", 0)
+		--         call nvim_win_set_option(s:win, "winhl", "Normal:DashboardImage")
+		--         terminal cat ~/Downloads/ascii.txt
+		--         :exe "normal \<C-W>\<C-w>"
+		--       endfunction
+		--     ]])
+		-- end, {})
 	end,
 }
