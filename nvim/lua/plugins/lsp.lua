48a92eea743b003bcd60da99c98d09d33d1f2e0b
sohei.suzuki <suzuki.s1008@gmail.com>
2024-06-04 00:06:09 +0900

    merge mason_lspconfig.lua and mason_null_ls.lua to lsp.lua

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -144,4 +144,62 @@ return {
 			vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", { fg = DgsHintHl.fg })
 		end,
 	},
+	{
+		"jay-babu/mason-null-ls.nvim",
+		dependencies = {
+			"nvimtools/none-ls.nvim",
+			"williamboman/mason.nvim",
+		},
+		event = "VeryLazy",
+		opts = {
+			ensure_installed = {
+				"stylua",
+				"prettier",
+				"black",
+			},
+			automatic_installation = true,
+			methods = {
+				diagnostics = true,
+				formatting = true,
+				code_actions = true,
+				completion = true,
+				hover = true,
+			},
+		},
+		config = function(_, opts)
+			local formatter_on_save = true
+			require("mason-null-ls").setup(opts)
+			local null_ls = require("null-ls")
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.formatting.prettier,
+					null_ls.builtins.formatting.black,
+				},
+				on_attach = function(client, bufnr)
+					if client.supports_method("textDocument/formatting") then
+						vim.api.nvim_create_autocmd("BufWritePre", {
+							callback = function()
+								if formatter_on_save then
+									vim.lsp.buf.format({ async = false })
+								end
+							end,
+							buffer = bufnr,
+						})
+					end
+				end,
+			})
+
+			-- User command
+			vim.api.nvim_create_user_command("Format", function()
+				vim.lsp.buf.format({ async = false })
+			end, {})
+			vim.api.nvim_create_user_command("FormatterEnable", function()
+				formatter_on_save = true
+			end, {})
+			vim.api.nvim_create_user_command("FormatterDisable", function()
+				formatter_on_save = false
+			end, {})
+		end,
+	},
 }
