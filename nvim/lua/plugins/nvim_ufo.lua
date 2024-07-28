c0e33a684c8ee474dc63444d7beb3551740f535f
Sohei.Suzuki <suzuki.s1008@gmail.com>
2024-07-28 17:52:41 +0900

    feat: add nvim-ufo

---
+++ nvim/lua/plugins/nvim_ufo.lua
@@ -0,0 +1,73 @@
+return {
+	"szkny/nvim-ufo",
+	dependencies = { "kevinhwang91/promise-async" },
+	opts = {
+		filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
+	},
+	config = function(_, opts)
+		vim.o.foldcolumn = "1" -- '0' is not bad
+		vim.o.foldlevel = 99
+		vim.o.foldlevelstart = 99
+		vim.o.foldenable = true
+    vim.o.fillchars = [[vert:│,eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+    vim.o.statuscolumn = '%=%s%l %#FoldColumn#%{foldlevel(v:lnum) > foldlevel(v:lnum - 1) ? (foldclosed(v:lnum) == -1 ? " " : " ") : "  " }%*'
+
+		local ufo = require("ufo")
+
+		vim.keymap.set("n", "zR", ufo.openAllFolds)
+		vim.keymap.set("n", "zM", ufo.closeAllFolds)
+
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities.textDocument.foldingRange = {
+			dynamicRegistration = false,
+			lineFoldingOnly = true,
+		}
+		local language_servers = require("lspconfig").util.available_servers()
+		for _, ls in ipairs(language_servers) do
+			require("lspconfig")[ls].setup({
+				capabilities = capabilities,
+			})
+		end
+
+    opts.provider_selector = function(_, _, _)
+		  return { "treesitter", "indent" }
+		end
+
+		vim.api.nvim_create_autocmd("FileType", {
+			group = vim.api.nvim_create_augroup("local_detach_ufo", { clear = true }),
+			pattern = opts.filetype_exclude,
+			callback = function()
+				ufo.detach()
+			end,
+		})
+
+		opts.fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
+			local newVirtText = {}
+			local suffix = ("  %d "):format(endLnum - lnum)
+			local sufWidth = vim.fn.strdisplaywidth(suffix)
+			local targetWidth = width - sufWidth
+			local curWidth = 0
+			for _, chunk in ipairs(virtText) do
+				local chunkText = chunk[1]
+				local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+				if targetWidth > curWidth + chunkWidth then
+					table.insert(newVirtText, chunk)
+				else
+					chunkText = truncate(chunkText, targetWidth - curWidth)
+					local hlGroup = chunk[2]
+					table.insert(newVirtText, { chunkText, hlGroup })
+					chunkWidth = vim.fn.strdisplaywidth(chunkText)
+					if curWidth + chunkWidth < targetWidth then
+						suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+					end
+					break
+				end
+				curWidth = curWidth + chunkWidth
+			end
+			table.insert(newVirtText, { suffix, "MoreMsg" })
+			return newVirtText
+		end
+
+		ufo.setup(opts)
+	end,
+}
