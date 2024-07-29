fe2f01e88854fda41c1bc439c56a68f09ce3eeba
Sohei.Suzuki <sohei.suzuki@accenture.com>
2024-07-29 17:14:31 +0900

    refactor: nvim-ufo setup

--- nvim/lua/plugins/nvim_ufo.lua
+++ nvim/lua/plugins/nvim_ufo.lua
@@ -3,6 +3,35 @@ return {
 	dependencies = { "kevinhwang91/promise-async" },
 	opts = {
 		filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
+    provider_selector = function(_, _, _)
+		  return { "treesitter", "indent" }
+		end,
+    fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
+			local newVirtText = {}
+			local suffix = (" 󰟃  %d "):format(endLnum - lnum)
+			local sufWidth = vim.fn.strdisplaywidth(suffix)
+			local targetWidth = width - sufWidth
+			local curWidth = 0
+			for _, chunk in ipairs(virtText) do
+				local chunkText = chunk[1]
+				local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+				if targetWidth > curWidth + chunkWidth then
+					table.insert(newVirtText, chunk)
+				else
+					chunkText = truncate(chunkText, targetWidth - curWidth)
+					local hlGroup = chunk[2]
+					table.insert(newVirtText, { chunkText, hlGroup })
+					chunkWidth = vim.fn.strdisplaywidth(chunkText)
+					if curWidth + chunkWidth < targetWidth then
+						suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+					end
+					break
+				end
+				curWidth = curWidth + chunkWidth
+			end
+			table.insert(newVirtText, { suffix, "MoreMsg" })
+			return newVirtText
+		end
 	},
 	config = function(_, opts)
 		vim.o.foldcolumn = "1" -- '0' is not bad
@@ -29,10 +58,6 @@ return {
 			})
 		end
 
-    opts.provider_selector = function(_, _, _)
-		  return { "treesitter", "indent" }
-		end
-
 		vim.api.nvim_create_autocmd("FileType", {
 			group = vim.api.nvim_create_augroup("local_detach_ufo", { clear = true }),
 			pattern = opts.filetype_exclude,
@@ -40,33 +65,6 @@ return {
 				ufo.detach()
 			end,
 		})
-
-		opts.fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
-			local newVirtText = {}
-			local suffix = ("  %d "):format(endLnum - lnum)
-			local sufWidth = vim.fn.strdisplaywidth(suffix)
-			local targetWidth = width - sufWidth
-			local curWidth = 0
-			for _, chunk in ipairs(virtText) do
-				local chunkText = chunk[1]
-				local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-				if targetWidth > curWidth + chunkWidth then
-					table.insert(newVirtText, chunk)
-				else
-					chunkText = truncate(chunkText, targetWidth - curWidth)
-					local hlGroup = chunk[2]
-					table.insert(newVirtText, { chunkText, hlGroup })
-					chunkWidth = vim.fn.strdisplaywidth(chunkText)
-					if curWidth + chunkWidth < targetWidth then
-						suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
-					end
-					break
-				end
-				curWidth = curWidth + chunkWidth
-			end
-			table.insert(newVirtText, { suffix, "MoreMsg" })
-			return newVirtText
-		end
 
 		ufo.setup(opts)
 	end,
