c38b27230a539071105dcdf62949ebc4dc975cff
Sohei.Suzuki <suzuki.s1008@gmail.com>
2024-08-10 17:21:58 +0900

    refactor fugitive setup

--- nvim/lua/plugins/vim_fugitive.lua
+++ nvim/lua/plugins/vim_fugitive.lua
@@ -2,21 +2,21 @@ return {
   "tpope/vim-fugitive",
   event = "CmdlineEnter",
   config = function()
-    vim.cmd([[
-      if exists('*fugitive#statusline')
-        set statusline+=%{fugitive#statusline()}
-      endif
-      fun! s:fugitive_init() abort
-        setlocal nonumber
-        setlocal bufhidden=wipe
-        setlocal nobuflisted
-        setlocal nolist
-        setlocal nospell
-        setlocal noequalalways
-        resize 10
-      endf
-      au FileType fugitive call s:fugitive_init()
-    ]])
+    vim.api.nvim_create_autocmd({ "FileType" }, {
+      pattern = { "fugitive" },
+      group = vim.api.nvim_create_augroup("custom_fugitive", { clear = true }),
+      callback = function()
+        vim.opt_local.number = false
+        vim.opt_local.bufhidden = "wipe"
+        vim.opt_local.buflisted = false
+        vim.opt_local.list = false
+        vim.opt_local.equalalways = false
+        vim.opt_local.spell = true
+        vim.cmd("resize 10")
+        -- local opts = { noremap = true, silent = true, nowait = true }
+        -- vim.api.nvim_buf_set_keymap(0, "n", "<Leader>", "<Plug>(fugitive_-)", opts)
+      end,
+    })
     -- vim.keymap.set("n", "<Leader>gg", ":<C-u>G<CR>", { noremap = true, silent = true })
     vim.keymap.set(
       "n",
