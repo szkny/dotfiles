112bc88337da67f2719f33530bb3a5a77ea12d7b
Sohei.Suzuki <suzuki.s1008@gmail.com>
2024-08-14 15:00:10 +0900

    setup nvim-dap setup for python

--- nvim/lua/plugins/nvim_dap.lua
+++ nvim/lua/plugins/nvim_dap.lua
@@ -1,51 +1,43 @@
 return {
-	"mfussenegger/nvim-dap",
-	event = "VeryLazy",
-	config = function()
-		local dap = require("dap")
-		dap.adapters.python = function(cb, config)
-			local python_cmd = "python"
-			if vim.fn.executable("poetry") == 1 then
-				python_cmd = "poetry run python"
-			end
-			if config.request == "attach" then
-				---@diagnostic disable-next-line: undefined-field
-				local port = (config.connect or config).port
-				---@diagnostic disable-next-line: undefined-field
-				local host = (config.connect or config).host or "127.0.0.1"
-				cb({
-					type = "server",
-					port = assert(port, "`connect.port` is required for a python `attach` configuration"),
-					host = host,
-					options = {
-						source_filetype = "python",
-					},
-				})
-			else
-				cb({
-					type = "executable",
-					command = python_cmd,
-					-- args = { "-m", "debugpy.adapter" },
-					args = { "-m", "debugpy" },
-					options = {
-						source_filetype = "python",
-					},
-				})
-			end
-		end
-		dap.configurations.python = {
-			{
-				type = "python",
-				request = "launch",
-				name = "Launch file",
-				program = "${file}",
-				pythonPath = function()
-					if vim.fn.executable("poetry") == 1 then
-						return vim.fn.system("poetry run which python")
-					end
-					return vim.fn.system("which python")
-				end,
-			},
-		}
-	end,
+  {
+    "mfussenegger/nvim-dap-python",
+    ft = "python",
+    dependencies = { "mfussenegger/nvim-dap" },
+    config = function ()
+      -- Python virtual env
+      local venv_path = vim.fn.trim(vim.fn.system("poetry env info -p"))
+      local python_path = ""
+      if vim.v.shell_error == 0 then
+        vim.env.VIRTUAL_ENV = venv_path
+        vim.env.PATH = venv_path .. "/bin:" .. vim.env.PATH
+        python_path = venv_path .. "/bin/python"
+      else
+        python_path = vim.g.python3_host_prog
+      end
+      require("dap-python").setup(python_path)
+      -- If using the above, then `/path/to/venv/bin/python -m debugpy --version`
+      -- must work in the shell
+    end
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    config = function ()
+      local dap, dapui = require("dap"), require("dapui")
+      dapui.setup()
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+    end
+  },
 }
