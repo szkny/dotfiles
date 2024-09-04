a18e83577b477c71c814b01824efaa70d0ddc561
Sohei.Suzuki <sohei.suzuki@accenture.com>
2024-09-04 17:18:07 +0900

    fix: neoscroll.nvim setup

--- nvim/lua/plugins/neoscroll.lua
+++ nvim/lua/plugins/neoscroll.lua
@@ -1,21 +1,16 @@
 return {
   "karb94/neoscroll.nvim",
   event = "VeryLazy",
-  config = function()
-    require("neoscroll").setup({
-      mappings = {
-        "<C-u>",
-        "<C-d>",
-      },
-      hide_cursor = true,
-      stop_eof = true,
-      respect_scrolloff = true,
-      cursor_scrolls_alone = true,
-      easing_function = "quadratic",
-    })
-    require("neoscroll.config").set_mappings({
-      ["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "300" } },
-      ["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "300" } },
-    })
-  end,
+  opts = {
+    mappings = {
+      "<C-u>",
+      "<C-d>",
+    },
+    hide_cursor = true,
+    stop_eof = true,
+    respect_scrolloff = true,
+    cursor_scrolls_alone = true,
+    easing_function = "quadratic",
+    performance_mode = false,
+  },
 }
