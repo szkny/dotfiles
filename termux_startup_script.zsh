cdeb59175dac1174d9de2f3f6855a913197a6574
Sohei.Suzuki <suzuki.s1008@gmail.com>
2024-09-23 01:22:47 +0900

    update termux startup script

--- termux_startup_script.zsh
+++ termux_startup_script.zsh
@@ -20,7 +20,16 @@ pkg install -y golang
 pkg install -y nodejs
 pkg install -y rust
 pkg install -y rustc cargo rustc-nightly
+pkg install -y neovim
 pkg install -y ranger w3m lynx
+
+# font
+pkg install -y lsd
+mkdir -p ~/Project/font
+cd ~/Project/font
+wget https://github.com/yuru7/HackGen/releases/download/v2.9.0/HackGen_NF_v2.9.0.zip
+unzip ./HackGen*.zip
+cp ./HackGen*/HackGenConsoleNF-Regular.ttf ~/.termux/font.ttf
 
 # for deno
 pacman-key --init
