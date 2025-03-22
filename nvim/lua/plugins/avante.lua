f444d645da8d5febee137f2585310d7dae6b70c3
Sohei.Suzuki <suzuki.s1008@gmail.com>
2025-03-22 23:18:01 +0900

    feat: 🎸 add avante.nvim

---
+++ nvim/lua/plugins/avante.lua
@@ -0,0 +1,53 @@
+return {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  version = false, -- Never set this value to "*"! Never!
+  opts = {
+    provider = "openai",
+    openai = {
+      endpoint = "https://api.openai.com/v1",
+      model = "gpt-4o",
+      timeout = 30000,
+      temperature = 0,
+      max_completion_tokens = 8192,
+      --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+    },
+  },
+  build = "make BUILD_FROM_SOURCE=true",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "hrsh7th/nvim-cmp",
+    "ibhagwan/fzf-lua",
+    "nvim-tree/nvim-web-devicons",
+    "zbirenbaum/copilot.lua",
+    {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
+  config = function(_, opts)
+    require("avante_lib").load()
+    require("avante").setup(opts)
+  end,
+}
