d652a60c6848a207b619d128cab90762f114dfe7
Sohei.Suzuki <suzuki.s1008@gmail.com>
2025-07-05 16:41:29 +0900

    refactor: 💡 replace tab to spaces in toggleterm.lua

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,52 +1,52 @@
 return {
-	"akinsho/toggleterm.nvim",
-	cmd = {
-		"ToggleTerm",
-		"ToggleTermToggleAll",
-	},
-	keys = {
-		{ "t", "<CMD>ToggleTerm<CR>", mode = "n" },
-		{ "<leader>gg", "<CMD>LazyGit<CR>", mode = "n" },
-	},
-	event = "VeryLazy",
-	version = "*",
-	config = function()
-		local FloatBorderHl = vim.api.nvim_get_hl(0, { name = "FloatBorder" })
-		require("toggleterm").setup({
-			-- direction = "float",
-			direction = "horizontal",
+  "akinsho/toggleterm.nvim",
+  cmd = {
+    "ToggleTerm",
+    "ToggleTermToggleAll",
+  },
+  keys = {
+    { "t", "<CMD>ToggleTerm<CR>", mode = "n" },
+    { "<leader>gg", "<CMD>LazyGit<CR>", mode = "n" },
+  },
+  event = "VeryLazy",
+  version = "*",
+  config = function()
+    local FloatBorderHl = vim.api.nvim_get_hl(0, { name = "FloatBorder" })
+    require("toggleterm").setup({
+      -- direction = "float",
+      direction = "horizontal",
       size = 20,
-			float_opts = {
-				border = "curved",
-			},
-			highlights = {
-				FloatBorder = {
-					guifg = FloatBorderHl.fg,
-					guibg = "none",
-				},
-			},
-			on_open = function()
-				vim.cmd([[startinsert]])
-			end,
-		})
-		local Terminal = require("toggleterm.terminal").Terminal
-		local lazygit = Terminal:new({
-			cmd = "lazygit",
-			hidden = true,
-			direction = "float",
-		})
-		vim.api.nvim_create_user_command("LazyGit", function()
-			lazygit:toggle()
-		end, {})
-		vim.api.nvim_create_user_command("Viu", function(opts)
-			local file = opts.fargs[1]
-			if file then
-				Terminal:new({
-					cmd = "viu -t " .. file .. "; read -q",
-					hidden = true,
-					direction = "float",
-				}):open()
-			end
-		end, { nargs = 1 })
-	end,
+      float_opts = {
+        border = "curved",
+      },
+      highlights = {
+        FloatBorder = {
+          guifg = FloatBorderHl.fg,
+          guibg = "none",
+        },
+      },
+      on_open = function()
+        vim.cmd([[startinsert]])
+      end,
+    })
+    local Terminal = require("toggleterm.terminal").Terminal
+    local lazygit = Terminal:new({
+      cmd = "lazygit",
+      hidden = true,
+      direction = "float",
+    })
+    vim.api.nvim_create_user_command("LazyGit", function()
+      lazygit:toggle()
+    end, {})
+    vim.api.nvim_create_user_command("Viu", function(opts)
+      local file = opts.fargs[1]
+      if file then
+        Terminal:new({
+          cmd = "viu -t " .. file .. "; read -q",
+          hidden = true,
+          direction = "float",
+        }):open()
+      end
+    end, { nargs = 1 })
+  end,
 }
