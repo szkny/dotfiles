15b24bc5d0d1ed31d1c5de956439c4759a502966
sohei.suzuki <suzuki.s1008@gmail.com>
2026-02-27 02:48:25 +0900

    feat: 🎸 add sidekick setup for auto-session

--- nvim/lua/plugins/auto_session.lua
+++ nvim/lua/plugins/auto_session.lua
@@ -1,129 +1,151 @@
 return {
-	"rmagatti/auto-session",
-	lazy = false,
+  "rmagatti/auto-session",
+  lazy = false,
   commit = "50f5f2ea",
-	opts = {
-		log_level = "error",
-		auto_session_root_dir = vim.fn.stdpath("data") .. "/sessions/",
-		auto_session_enabled = true,
-		auto_session_create_enabled = true,
-		auto_session_enable_last_session = false,
-		auto_session_suppress_dirs = { "~/", "~/Project", "~/Downloads", "/" },
-		auto_save_enabled = true,
-		auto_restore_enabled = true,
-		auto_session_use_git_branch = false,
-		bypass_session_save_file_types = nil,
-		cwd_change_handling = {
-			restore_upcoming_session = true,
-			pre_cwd_changed_hook = nil,
-			post_cwd_changed_hook = nil,
-		},
-	},
-	config = function(_, opts)
-    vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+  opts = {
+    log_level = "error",
+    auto_session_root_dir = vim.fn.stdpath("data") .. "/sessions/",
+    auto_session_enabled = true,
+    auto_session_create_enabled = true,
+    auto_session_enable_last_session = false,
+    auto_session_suppress_dirs = { "~/", "~/Project", "~/Downloads", "/" },
+    auto_save_enabled = true,
+    auto_restore_enabled = true,
+    auto_session_use_git_branch = false,
+    bypass_session_save_file_types = nil,
+    cwd_change_handling = {
+      restore_upcoming_session = true,
+      pre_cwd_changed_hook = nil,
+      post_cwd_changed_hook = nil,
+    },
+  },
+  config = function(_, opts)
+    vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,localoptions"
 
-		require("auto-session").setup(opts)
-		local auto_session_plugins = {
-			splitterm = {
-				state = false,
-				save_index = 1,
-				save_commands = {
-					"exe '18SplitTerm'",
-					'exe "normal! \\<C-w>W"',
-				},
-			},
-			neotree = {
-				state = false,
-				save_index = 2,
-				save_commands = {
-					"exe 'Neotree'",
-					'exe "normal! \\<C-w>W"',
-				},
-			},
-			aerial = {
-				state = false,
-				save_index = 3,
-				save_commands = { "exe 'AerialToggle!'" },
-			},
-			trouble = {
-				state = false,
-				save_index = 4,
-				save_commands = { "exe 'Trouble diagnostics'" },
-			},
-		}
+    require("auto-session").setup(opts)
+    local auto_session_plugins = {
+      splitterm = {
+        state = false,
+        save_index = 1,
+        save_commands = {
+          "exe '18SplitTerm'",
+          'exe "normal! \\<C-w>W"',
+        },
+      },
+      neotree = {
+        state = false,
+        save_index = 2,
+        save_commands = {
+          "exe 'Neotree'",
+          'exe "normal! \\<C-w>W"',
+        },
+      },
+      aerial = {
+        state = false,
+        save_index = 3,
+        save_commands = { "exe 'AerialToggle!'" },
+      },
+      trouble = {
+        state = false,
+        save_index = 4,
+        save_commands = { "exe 'Trouble diagnostics'" },
+      },
+      sidekick = {
+        state = false,
+        save_index = 5,
+        save_commands = {
+          'lua require("sidekick.cli").toggle({ name = "gemini", focus = true })'
+        }
+      }
+    }
 
-		function Find_buffer(pattern)
-			local buffers = vim.api.nvim_list_bufs()
-			for _, buf in ipairs(buffers) do
-				local bufname = vim.api.nvim_buf_get_name(buf)
-				if string.find(bufname, pattern) then
-					return true
-				end
-			end
-			print(pattern)
-			return false
-		end
-
-		local function close_splitterm()
-			if Find_buffer("SplitTerm") then
-				vim.cmd("SplitTermClose")
-				auto_session_plugins.splitterm.state = true
-			end
-		end
-		local function close_neo_tree()
-			if Find_buffer("tree filesystem") then
-				vim.cmd("Neotree close")
-				auto_session_plugins.neotree.state = true
-			end
-		end
-		local function close_aerial()
-			local aerial_api = require("aerial")
-			if aerial_api.is_open() then
-				aerial_api.close_all()
-				auto_session_plugins.aerial.state = true
-			end
-		end
-		local function close_trouble()
-			local trouble_api = require("trouble")
-			if trouble_api.is_open() then
-				trouble_api.close()
-				auto_session_plugins.trouble.state = true
-			end
-		end
-		local function save_auto_session()
-			local auto_session_root_dir = require("auto-session").get_root_dir()
-			local cwd = string.gsub(vim.fn.getcwd(), "/", "%%")
-			local auto_session_file_name = auto_session_root_dir .. cwd .. ".vim"
-			local auto_session_file_io = io.open(auto_session_file_name, "a")
-			if auto_session_file_io then
-				local sorted_plugins = {}
-				for _, plugin in pairs(auto_session_plugins) do
-					table.insert(sorted_plugins, plugin)
-				end
-				table.sort(sorted_plugins, function(a, b)
-					return a.save_index < b.save_index
-				end)
-				for _, plugin in ipairs(sorted_plugins) do
-					if plugin.state then
-						plugin.state = false
-						for _, command in ipairs(plugin.save_commands) do
-							auto_session_file_io:write(command .. "\n")
-						end
-					end
-				end
-				auto_session_file_io:close()
-			else
-				print("Failed to open file: " .. auto_session_file_name)
-			end
-		end
-		vim.api.nvim_create_autocmd("ExitPre", {
-			callback = function()
-				close_splitterm()
-				close_neo_tree()
-				close_aerial()
-				close_trouble()
-			end,
-		})
-		vim.api.nvim_create_autocmd({ "VimLeave" }, { callback = save_auto_session })
-	end,
+    local function find_buffer(pattern)
+      local buffers = vim.api.nvim_list_bufs()
+      for _, buf in ipairs(buffers) do
+        local bufname = vim.api.nvim_buf_get_name(buf)
+        if string.find(bufname, pattern) then
+          return true
+        end
+      end
+      print(pattern)
+      return false
+    end
+    local function find_buffer_by_filetype(ftype)
+      for _, win in ipairs(vim.api.nvim_list_wins()) do
+        local buf = vim.api.nvim_win_get_buf(win)
+        if vim.bo[buf].filetype == ftype then
+          return true
+        end
+      end
+      return false
+    end
+    local function close_splitterm()
+      if find_buffer("SplitTerm") then
+        vim.cmd("SplitTermClose")
+        auto_session_plugins.splitterm.state = true
+      end
+    end
+    local function close_neo_tree()
+      if find_buffer("tree filesystem") then
+        vim.cmd("Neotree close")
+        auto_session_plugins.neotree.state = true
+      end
+    end
+    local function close_aerial()
+      local aerial_api = require("aerial")
+      if aerial_api.is_open() then
+        aerial_api.close_all()
+        auto_session_plugins.aerial.state = true
+      end
+    end
+    local function close_trouble()
+      local trouble_api = require("trouble")
+      if trouble_api.is_open() then
+        trouble_api.close()
+        auto_session_plugins.trouble.state = true
+      end
+    end
+    local function close_sidekick()
+      if find_buffer_by_filetype("sidekick_terminal") then
+        require("sidekick.cli").close()
+        auto_session_plugins.sidekick.state = true
+      end
+    end
+    local function save_auto_session()
+      local auto_session_root_dir = require("auto-session").get_root_dir()
+      local cwd = string.gsub(vim.fn.getcwd(), "/", "%%")
+      local auto_session_file_name = auto_session_root_dir .. cwd .. ".vim"
+      local auto_session_file_io = io.open(auto_session_file_name, "a")
+      if auto_session_file_io then
+        local sorted_plugins = {}
+        for _, plugin in pairs(auto_session_plugins) do
+          table.insert(sorted_plugins, plugin)
+        end
+        table.sort(sorted_plugins, function(a, b)
+          return a.save_index < b.save_index
+        end)
+        for _, plugin in ipairs(sorted_plugins) do
+          if plugin.state then
+            plugin.state = false
+            for _, command in ipairs(plugin.save_commands) do
+              auto_session_file_io:write(command .. "\n")
+            end
+          end
+        end
+        auto_session_file_io:close()
+      else
+        print("Failed to open file: " .. auto_session_file_name)
+      end
+    end
+    vim.api.nvim_create_autocmd("ExitPre", {
+      callback = function()
+        close_splitterm()
+        close_neo_tree()
+        close_aerial()
+        close_trouble()
+        close_sidekick()
+      end,
+    })
+    vim.api.nvim_create_autocmd({ "VimLeave" }, { callback = save_auto_session })
+  end,
 }
